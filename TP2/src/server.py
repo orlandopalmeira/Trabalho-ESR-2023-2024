77fbf7246947d15eaef421c8af26b920512d20b6
Katilho <pemicama12@gmail.com>
2023-10-26 19:17:11 +0100

    removed the 2 server files, only 1 server

--- TP2/src/server.py
+++ TP2/src/server.py
@@ -1,7 +1,10 @@
 import socket
 import sys
 import time
-import threading
+# import threading
+import multiprocessing
+import signal
+import os
 
 class Server:
     def __init__(self, ip:str, port:int):
@@ -9,6 +12,7 @@ class Server:
         self.port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # SOCK_STREAM = TCP; SOCK_DGRAM = UDP
         self.server_socket.bind((self.ip, self.port))
+        # sockets_open.append(self.server_socket)
 
     def listen(self):
         self.server_socket.listen()
@@ -36,7 +40,8 @@ class Server:
         ####Por um dicionario a ser enviado e recebido de forma serializada
         print(f"Received data from client: {data}")
         response = "Hello from the server"
-        # time.sleep(10) # Para simular processamento demorado
+        time.sleep(5) # Para simular processamento demorado
+
         self.send(client_socket, response)
 
         # Fecha o socket do cliente
@@ -47,21 +52,46 @@ class Server:
         self.server_socket.close()
 
 
-if __name__ == "__main__":
-    server_ip = '10.0.4.10'
-    server_port = 3000
-
+def attend_clients(server_ip:str, server_port:int):
+    print(f'ID process on port {server_port}: {os.getpid()}')
+    # server_ip = '10.0.4.10'
+    # server_port = 3000
     server = Server(server_ip, server_port)
-    try:
-
-        server.listen()
-
-        while True:
+    server.listen()
+    run = True
+    while run:
+        try:
             client_socket, client_address = server.accept()
-
-            #* Cria uma thread para tratar do cliente
-            client_thread = threading.Thread(target=server.handle_client, args=(client_socket, client_address))
+            # Cria uma thread para tratar do cliente
+            client_thread = multiprocessing.Process(target=server.handle_client, args=(client_socket, client_address))
             client_thread.start()
+        except Exception:
+            run = False
+            print(f"{server_port} fechado")
+            server.close()
+
+services = []
+
+def ctrcl_handler(sig, frame):
+    global services
+    print(f'You pressed Ctrl+C!, process id {os.getpid()}')
+    for s in services: s.terminate()
+    sys.exit(0)
+
+def main():
+    print(f'ID master process: {os.getpid()}')
+    signal.signal(signal.SIGINT, ctrcl_handler)
+    t1 = multiprocessing.Process(target=attend_clients, args=('10.0.4.10', 3000))
+    services.append(t1)
+    t1.start()
+    t2 = multiprocessing.Process(target=attend_clients, args=('10.0.4.10', 3001))
+    services.append(t2)
+    t2.start()
+    t3 = multiprocessing.Process(target=attend_clients, args=('10.0.4.10', 3002))
+    services.append(t3)
+    t3.start()
+
     
-    finally:
-        server.close()
+
+if __name__ == "__main__":
+    main()
