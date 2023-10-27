bfd459ea274d5c7459669895d40499d16904729d
Katilho <pemicama12@gmail.com>
2023-10-27 12:01:51 +0100

    organized the code better

--- TP2/src/server.py
+++ TP2/src/server.py
@@ -4,9 +4,14 @@ import signal
 import sys
 import time
 
+# Função para encerrar o servidor e as suas threads no momento do CTRL+C
+def ctrlc_handler(sig, frame):
+    print("A encerrar o servidor e as threads...")
+    sys.exit(0)
+
 # Função para lidar com os clientes do serviço svc_attend_clients
-def handle_attend_clients(client_socket, address, service_name):
-    print(f"Conexão estabelecida com {address} em {service_name}")
+def handle_attend_clients(client_socket, address):
+    print(f"Conexão estabelecida com {address}")
     while True:
         data = client_socket.recv(1024)
         if not data:
@@ -14,25 +19,22 @@ def handle_attend_clients(client_socket, address, service_name):
         response = f"{data.upper().decode('utf-8')}"
         time.sleep(3)
         client_socket.send(response.encode('utf-8'))
-    print(f"Conexão encerrada com {address} em {service_name}")
+    print(f"Conexão encerrada com {address}")
     client_socket.close()
 
 # Função que lida com o serviço svc_attend_clients
-def svc_attend_clients(service_name, port):
+def svc_attend_clients(service_name:str, port:int):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', port))
-    server_socket.listen(5)
+    endereco = '10.0.4.10'
+    server_socket.bind((endereco, port))
+    server_socket.listen()
     print(f"Serviço '{service_name}' pronto para receber conexões na porta {port}")
 
     while True:
         client_socket, addr = server_socket.accept()
-        client_handler = threading.Thread(target=handle_attend_clients, args=(client_socket, addr, service_name))
+        client_handler = threading.Thread(target=handle_attend_clients, args=(client_socket, addr))
         client_handler.start()
 
-# Função para encerrar o servidor e as suas threads no momento do CTRL+C
-def ctrlc_handler(sig, frame):
-    print("A encerrar o servidor e as threads...")
-    sys.exit(0)
 
 def main():
     # Regista o sinal para encerrar o servidor no momento do CTRL+C
@@ -43,19 +45,17 @@ def main():
     service2_thread = threading.Thread(target=svc_attend_clients, args=('Serviço 2', 3001))
     service3_thread = threading.Thread(target=svc_attend_clients, args=('Serviço 3', 3002))
 
-    service1_thread.daemon = True
-    service2_thread.daemon = True
-    service3_thread.daemon = True
+    threads = [service1_thread, service2_thread, service3_thread]
 
-    # Inicia os serviços
-    service1_thread.start()
-    service2_thread.start()
-    service3_thread.start()
+    for t in threads:
+        t.daemon = True
+    
+    for t in threads:
+        t.start()
 
-    # Aguarda até que todas as threads terminem
-    service1_thread.join()
-    service2_thread.join()
-    service3_thread.join()
+    for t in threads:
+        t.join()
+
 
 if __name__ == '__main__':
     main()
