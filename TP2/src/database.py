c424e7adcdbb0a85ba18f77310f5064d7f4f8e41
Katilho <pemicama12@gmail.com>
2023-10-27 13:17:19 +0100

    server reads config file for db

--- TP2/src/database.py
+++ TP2/src/database.py
@@ -1,4 +1,5 @@
 import threading
+import json
 
 class Database:
     # shared_variable = 0  # This is a class variable
@@ -9,21 +10,34 @@ class Database:
         self.vizinhos = set()
         self.vizinhoslock = threading.Lock()
 
+    # L~e o ficheiro de configuração JSON e guarda o necessário na base de dados
+    def read_config_file(self, filepath):
+        with open(filepath) as f:
+            data = json.load(f)
+            self.vizinhoslock.acquire()
+            self.vizinhos = set(data["vizinhos"])
+            self.vizinhoslock.release()
+
     def add_vizinho(self, ip):
         self.vizinhoslock.acquire()
         self.vizinhos.add(ip)
         self.vizinhoslock.release()
 
     def remove_vizinho(self, ip):
-        self.vizinhoslock.acquire()
-        self.vizinhos.remove(ip)
-        self.vizinhoslock.release()
+        with self.vizinhoslock:
+            try:
+                self.vizinhos.remove(ip)
+                return "Vizinho removido com sucesso"
+            except KeyError:
+                return "Vizinho não existia"
 
     # Printa os vizinhos
     def show_vizinhos(self):
         self.vizinhoslock.acquire()
+        print()
         print(f"Vizinhos: {self.vizinhos}")
         print(f"Quantidade: {len(self.vizinhos)}")
+        print()
         self.vizinhoslock.release()
     
     def quantos_vizinhos(self):
@@ -38,7 +52,10 @@ class Database:
         self.videoslock.release()
 
     def remove_video(self, video_id):
-        self.videoslock.acquire()
-        self.videos.remove(video_id)
-        self.videoslock.release()
+        with self.videoslock:
+            try:
+                self.videos.remove(video_id)
+                return "Vídeo removido com sucesso"
+            except KeyError:
+                return "Video não existia"
 
--- TP2/src/server.py
+++ TP2/src/server.py
@@ -10,6 +10,7 @@ def ctrlc_handler(sig, frame):
     print("A encerrar o servidor e as threads...")
     sys.exit(0)
 
+##################################################################################################################
 
 # Função para lidar com os clientes do serviço svc_attend_clients
 def handle_attend_clients(client_socket, address:tuple, db: Database):
@@ -38,8 +39,9 @@ def svc_attend_clients(port:int, db: Database):
         client_handler = threading.Thread(target=handle_attend_clients, args=(client_socket, addr, db))
         client_handler.start()
 
+##################################################################################################################
 
-# Função para lidar com os clientes do serviço de atender clientes. !Genérico!
+# Função para lidar com o serviço svc_add_vizinhos
 def handle_add_vizinhos(client_socket, address:tuple, db: Database):
     print(f"Conexão estabelecida com {address}")
     while True:
@@ -70,27 +72,67 @@ def svc_add_vizinhos(port:int, db: Database):
         client_handler = threading.Thread(target=handle_add_vizinhos, args=(client_socket, addr, db))
         client_handler.start()
 
+##################################################################################################################
+
+# Função para lidar com o serviço svc_add_vizinhos
+def handle_remove_vizinhos(client_socket, address:tuple, db: Database):
+    print(f"Conexão estabelecida com {address}")
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+
+        #! Meter condição para dizer se o ip ja nao existia
+        response = db.remove_vizinho(address[0])
+        time.sleep(3)
+
+        client_socket.send(response.encode('utf-8'))
+        
+    print(f"Conexão encerrada com {address}")
+    client_socket.close()
+
+# Função que lida com o serviço de adicionar vizinhos
+def svc_remove_vizinhos(port:int, db: Database):
+    service_name = 'svc_remove_vizinhos'
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    endereco = '0.0.0.0' # Listen on all interfaces
+    server_socket.bind((endereco, port))
+    server_socket.listen()
+    print(f"Serviço '{service_name}' pronto para receber conexões na porta {port}")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        client_handler = threading.Thread(target=handle_remove_vizinhos, args=(client_socket, addr, db))
+        client_handler.start()
+
+##################################################################################################################
 
 # Função que lida com o serviço de mostrar vizinhos de 5 em 5 segundos
 def svc_show_vizinhos(db: Database):
     service_name = 'svc_show_vizinhos'
-    print(f"Serviço {service_name} pronto para mostrar vizinhos de 5 em 5 segundos.")
+    print(f"Serviço '{service_name}' pronto para mostrar vizinhos de 5 em 5 segundos.")
     while True:
         db.show_vizinhos()
         time.sleep(5)
 
 def main():
+
+    db = Database()
+    if len(sys.argv) < 2:
+        print(f"Uso: python3 {sys.argv[0]} <config_file.json>")
+        sys.exit(1)
+    db.read_config_file(sys.argv[1])
+
     # Regista o sinal para encerrar o servidor no momento do CTRL+C
     signal.signal(signal.SIGINT, ctrlc_handler)
 
-    db = Database()
-
     # Inicia os serviços em threads separadas
-    service1_thread = threading.Thread(target=svc_attend_clients, args=(3000, db))
-    service2_thread = threading.Thread(target=svc_add_vizinhos, args=(3001, db))
-    service3_thread = threading.Thread(target=svc_show_vizinhos, args=(db,))
+    svc1_thread = threading.Thread(target=svc_attend_clients, args=(3000, db))
+    svc2_thread = threading.Thread(target=svc_add_vizinhos, args=(3001, db))
+    svc3_thread = threading.Thread(target=svc_remove_vizinhos, args=(3002, db))
+    svc4_thread = threading.Thread(target=svc_show_vizinhos, args=(db,))
 
-    threads = [service1_thread, service2_thread, service3_thread]
+    threads = [svc1_thread, svc2_thread, svc3_thread, svc4_thread]
 
     for t in threads:
         t.daemon = True
